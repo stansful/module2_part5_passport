9651b97877e1cedbde665e5cd08ddd0caf4f218b
stansful <stansful@gmail.com>
2022-03-29 14:24:09 +0300

    feat: edit id type MODULE-2-PART-5-passport

--- backend/src/user/user_interfaces.ts
+++ backend/src/user/user_interfaces.ts
@@ -1,9 +1,11 @@
+import { Schema } from 'mongoose';
+
 export interface User {
   email: string;
   password: string;
 }
 
 export interface MongoResponseUser extends User {
-  _id: string;
+  _id: Schema.Types.ObjectId;
   __v: number;
 }
